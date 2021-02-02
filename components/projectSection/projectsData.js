01f63516ff099d04ac816702c8a18f998881b8bc
Sagi Twig <twigsagi@gmail.com>
2021-02-02 03:39:09 +0200

    Update devjobs project

--- components/projectSection/projectsData.js
+++ components/projectSection/projectsData.js
@@ -19,23 +19,23 @@ const projectsData = [
   },
   {
     id: 2,
-    title: "Perfect Plate",
-    description:
-      "Perfect plate is a website that lets you calculate the nutrition value of any given food and create your own plate with all the nutrition data.",
-    tags: ["react", "sass", "API", "adobe-xd", "axios"],
-    demo: "https://perfect-plate.netlify.app/",
-    github: "https://github.com/Sagi23/perfect-plate-v2",
-    image: "/perfect-plate-min.png",
-  },
-  {
-    id: 3,
     title: "Devjobs",
     description:
       "Devjobs is a website that lets you search for a tech job by the title of the job, location and more.",
-    tags: ["html", "sass", "js", "API", "adobe-xd", "axios"],
-    demo: "https://dev-search-jobs.netlify.app/",
-    github: "https://github.com/Sagi23/github-jobs",
-    image: "/dev-jobs1-min.png",
+    tags: ["react (CRA)", "styled-components", "API", "adobe-xd", "axios"],
+    demo: "https://devjobs1.netlify.app/",
+    github: "https://github.com/Sagi23/dev-jobs-react",
+    image: "/devjobs.png",
+  },
+  {
+    id: 3,
+    title: "Perfect Plate",
+    description:
+      "Perfect plate is a website that lets you calculate the nutrition value of any given food and create your own plate with all the nutrition data.",
+    tags: ["react (CRA)", "sass", "API", "adobe-xd", "axios"],
+    demo: "https://perfect-plate.netlify.app/",
+    github: "https://github.com/Sagi23/perfect-plate-v2",
+    image: "/perfect-plate-min.png",
   },
   {
     id: 4,
